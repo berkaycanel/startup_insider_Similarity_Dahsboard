00f242c9f285eed72db14d15e899727b57a21e6e
Berkay Canel <56683765+berkaycanel@users.noreply.github.com>
2024-10-24 14:48:09 +0200

    Update es.py

--- es.py
+++ es.py
@@ -87,7 +87,7 @@ def get_related_domains_new(tags, domain, funding_stage, employees, total_fundin
     if boosts is None:
         boosts = {
             
-            'refined_gpt_tags': 4.5,
+            'refined_gpt_tags': 9.5,
             'cb_tags': 2.5,
             'li_tags': 1.5,
             'funding_stage': 2.0,
